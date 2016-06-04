907c61cd6f3b6aee14d8d68e7f26ec88bffcd165
Tannin <sherb@gmx.net>
2016-06-04 20:36:04 +0200

    translation

---
+++ src/installer_bundle_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>InstallerBundle</name>
+    <message>
+        <location filename="installerbundle.cpp" line="50"/>
+        <source>Proxy-Installer to handle bundles containing the actual mod archive</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
