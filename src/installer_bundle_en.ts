3ab86a768b048a3cea46448edab87aa578536fe1
Mikaël Capelle <capelle.mikael@gmail.com>
2021-02-02 20:39:13 +0100

    Update translation file.

--- src/installer_bundle_en.ts
+++ src/installer_bundle_en.ts
@@ -4,8 +4,42 @@
 <context>
     <name>InstallerBundle</name>
     <message>
-        <location filename="installerbundle.cpp" line="50"/>
+        <location filename="installerbundle.cpp" line="48"/>
+        <source>Bundle Installer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installerbundle.cpp" line="58"/>
         <source>Proxy-Installer to handle bundles containing the actual mod archive</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MultiArchiveDialog</name>
+    <message>
+        <location filename="multiarchivedialog.ui" line="14"/>
+        <source>Bundled archives</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="multiarchivedialog.ui" line="20"/>
+        <source>This archive contains multiple archives that can be installed separately. Please select the archive you want to extract and install.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="multiarchivedialog.ui" line="35"/>
+        <location filename="multiarchivedialog.ui" line="38"/>
+        <source>Opens a Dialog that allows custom modifications.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="multiarchivedialog.ui" line="41"/>
+        <source>Manual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="multiarchivedialog.ui" line="64"/>
+        <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
